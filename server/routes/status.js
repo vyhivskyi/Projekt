faa66bb5d1850beaf1ebb3f53aafd268c573d811
vyhivskyi <rushoskey@gmail.com>
2023-11-12 20:18:01 +0100

    Status edited

--- server/routes/status.js
+++ server/routes/status.js
@@ -5,7 +5,7 @@ const Applications = require("../models/applications");
 router.get("/", async (req, res) => {
   try {
     const status = await Applications.findOne({user_id: req.user._id});
-    res.status(200).send({ data: status });
+    res.json(status);
   } catch (error) {
     console.log(req.user);
     res.status(500).send({ message: "Internal Server Error" });
